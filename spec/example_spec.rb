09f1e6f41a73ece16ae0e48063beffcbc2dd53ad
Tyler Mercier <tylermercier@gmail.com>
2013-04-23 19:01:01 -0600

    add test folder

---
+++ spec/example_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Thing" do
+  context "should" do
+    it "assert" do
+      true.should be_true
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+#require File.join(File.dirname(__FILE__), "..", "file.rb")
+require "rspec"
+
+RSpec.configure do |conf|
+end
